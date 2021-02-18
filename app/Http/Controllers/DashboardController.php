01a575f0f7593410c1516bd3deb6674ecbf78618
Simone Bogni <simone.bogni@gmail.com>
2021-02-18 19:45:03 +0100

    Fixed Surveys in Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -29,23 +29,36 @@ class DashboardController extends Controller
             return redirect(route('password.request'));
          }
          else{
-            $role = auth()->user()->role;
+            $user = auth()->user();
+            $role = $user->role;
             $users = 0;
             $patients = 0;
+            $surveysTotal = 0;
+            $surveysNew = 0;
             if($role == 'A'){
                 $users = User::count();
+                //questionari utenti
+                $surveysTotal = Survey::join('users', 'surveys.user_id', '=', 'users.id')->count();
             }
             if($role == 'P'){
                 $patients = User::where('user_id', auth()->user()->id)->count();
+                //questionari pazienti
+                $surveysTotal = Survey::join('users', 'surveys.user_id', '=', 'users.id')
+                    ->where('users.user_id', '=', $user->id)->count();
+                $surveysNew = Survey::join('users', 'surveys.user_id', '=', 'users.id')
+                    ->where('users.user_id', '=', $user->id)->whereNull('surveys.checked_at')->count();
+            }
+            if($role == 'U'){
+                //questionari utente
+                $surveysTotal = Survey::join('users', 'surveys.user_id', '=', 'users.id')
+                    ->where('surveys.user_id', '=', $user->id)->count();
             }
             return view('dashboard', [
-                'user' => auth()->user(),
+                'user' => $user,
                 'users' => $users,
                 'patients' => $patients,
-                'surveysTotal' => Survey::join('users', 'surveys.user_id', '=', 'users.id')
-                    ->whereNotNull('users.user_id')->count(),
-                'surveysNew' => Survey::join('users', 'surveys.user_id', '=', 'users.id')
-                ->whereNotNull('users.user_id')->whereNull('surveys.checked_at')->count()
+                'surveysTotal' => $surveysTotal,
+                'surveysNew' => $surveysNew
             ]);   
          }
     }
