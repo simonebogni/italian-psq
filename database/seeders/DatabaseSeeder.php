3fada32143e959cf9b1862e04f048951cdff44b2
Simone Bogni <simone.bogni@gmail.com>
2021-02-18 22:06:26 +0100

    Updated DatabaseSeeder with 2 pediatricians

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,28 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $now = \Carbon\Carbon::now();
-        \App\Models\User::factory(['role'=>'P'])->count(2)->create();
+        \App\Models\User::factory([
+            'role'=>'P',
+            'name' => 'Marco Rossi',
+            'email'=>'m.rossi@italianpsq.com',
+            'email_verified_at'=>$now,
+            'password' => '$2y$10$IyaLQoV8Nq8lAFUCUrWCTu85rJ7A7U2TdXH2tgK3l1CxiEvrqZxg.',
+            'password_changed_at' => $now,
+            'birth_date' => '2000-01-01 00:00:00',
+            'fiscal_code' => 'RSSMRC92R07L682G',
+            'phone_number' => '01234567890'
+            ])->count(1)->create();
+        \App\Models\User::factory([
+            'role'=>'P',
+            'name' => 'Giuseppe Verdi',
+            'email'=>'g.verdi@italianpsq.com',
+            'email_verified_at'=>$now,
+            'password' => '$2y$10$IyaLQoV8Nq8lAFUCUrWCTu85rJ7A7U2TdXH2tgK3l1CxiEvrqZxg.',
+            'password_changed_at' => $now,
+            'birth_date' => '2000-01-01 00:00:00',
+            'fiscal_code' => 'VRDGSP92R07L682G',
+            'phone_number' => '01234567890'
+            ])->count(1)->create();
         \App\Models\User::factory(['user_id'=>1])->count(5)->create();
         \App\Models\User::factory(['user_id'=>2])->count(6)->create();
         \App\Models\Survey::factory(['user_id'=>3])->count(3)->create();
