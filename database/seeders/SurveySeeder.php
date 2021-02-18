843b6c82d80229cd0a608491945c3e6ded90127c
Simone Bogni <simone.bogni@gmail.com>
2021-02-18 18:42:59 +0100

    Added Survey creation to DatabaseSeeder

--- database/seeders/SurveySeeder.php
+++ database/seeders/SurveySeeder.php
@@ -13,10 +13,5 @@ class SurveySeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Survey::factory(['user_id'=>3])->count(3)->create();
-        \App\Models\Survey::factory(['user_id'=>7])->count(1)->create();
-        \App\Models\Survey::factory(['user_id'=>8])->count(2)->create();
-
-
     }
 }
