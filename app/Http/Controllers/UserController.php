5063bcb22764db7535ee00aa43373b848061414a
Simone Bogni <simone.bogni@gmail.com>
2021-02-27 11:39:01 +0100

    Added role, name, email, password, phone to users.create. Added validation to store method and dumped request to the browser.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -40,10 +40,11 @@ class UserController extends Controller
     {
         $pediatricians = [];
         $roles = [];
-        switch (auth()->user()->role) {
+        $userRole = auth()->user()->role;
+        switch ($userRole) {
             case 'A':
                 $pediatricians = User::Pediatricians()->get();
-                $roles = [__("Admin"), __("Pediatrician"), __("Patient")];
+                $roles = [__("Patient"), __("Pediatrician"), __("Admin")];
                 break;
             case 'P':
                 $pediatricians = User::where('id', '=', auth()->user()->id)->get();
@@ -54,6 +55,7 @@ class UserController extends Controller
                 break;
         }
         return view('users.create', [
+            'userRole' => $userRole,
             'pediatricians' => $pediatricians,
             'roles' => $roles
         ]);
@@ -67,7 +69,18 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'pediatrician' => 'required',
+            'role' => 'required', 
+            'name' => 'required|max:255',
+            'email' => 'required|unique:App\Models\User,email|max:255',
+            'password' => 'required|regex:/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8, 20}$/i|same:passwordRepeat',
+            'passwordRepeat' => 'required|regex:/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8, 20}$/i|same:password',
+            'fiscalCode' => 'required|unique:App\Models\User,fiscal_code|regex:/^(?:[A-Z][AEIOU][AEIOUX]|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/i',
+            'phone' => 'required|numeric'
+
+        ]);
+        dd($request);
     }
 
     /**
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,25 +1,110 @@
 @extends('layouts.base')
 @section('main-content')
 
-<div class="row">
+<div class="row mt-3">
     <div class="col">
         <h1 class="h1 text-primary">{{__("User account creation")}}</h1>
     </div>
 </div>
 <form action="{{route('users.store')}}" method="post">
     @csrf
+    <div class="row bt-3">
+        <div class="col col-md-6">
+            <fieldset class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend"><label for="pediatrician" class="input-group-text">{{__("Pediatrician")}}</label></div>
+                    <select name="pediatrician" id="pediatrician" class="custom-select" required aria-required="true">
+                        @if ($userRole == 'A')
+                            <option value="null" selected>{{__("None")}}</option>
+                            @foreach ($pediatricians as $index => $pediatrician)
+                            <option value="{{$pediatrician->id}}">{{$pediatrician->name}} - {{$pediatrician->fiscal_code}}</option>
+                            @endforeach
+                        @else    
+                            @foreach ($pediatricians as $index => $pediatrician)
+                            <option value="{{$pediatrician->id}}" @if ($index == 0)
+                                selected
+                            @endif>{{$pediatrician->name}} - {{$pediatrician->fiscal_code}}</option>
+                            @endforeach
+                        @endif
+                    </select>
+                </div>
+            </fieldset>
+        </div>
+        <div class="col col-md-6">
+            <fieldset class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend"><label for="role" class="input-group-text">{{__("Role")}}</label></div>
+                    <select name="role" id="role" class="custom-select" required aria-required="true">
+                        @foreach ($roles as $index => $role)
+                        <option value="{{$role}}" @if ($index == 0)
+                            selected
+                        @endif>{{$role}}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </fieldset>
+        </div>
+    </div>
     <div class="row">
         <div class="col col-md-6">
             <fieldset class="form-group">
-                <div class="input-group bt-3">
-                    <div class="input-group-prepend"><label for="selectPediatrician" class="input-group-text">{{__("Pediatrician")}}</label></div>
-                    <select name="" id="selectPediatrician" class="custom-select">
-                        @foreach ($pediatricians as $index => $pediatrician)
-                        <option value="{{$pediatrician->id}}" @if ($index == 0)
-                            selected
-                        @endif>{{$pediatrician->name}} - {{$pediatrician->fiscal_code}}</option>
-                        @endforeach
-                    </select>
+                <div class="input-group">
+                    <div class="input-group-prepend"><label for="name" class="input-group-text">{{__("Name")}}</label></div>
+                    <input type="text" name="name" id="name" required aria-required="true" placeholder="Cognome Nome" aria-placeholder="Cognome Nome">
+                </div>
+            </fieldset>
+        </div>
+        <div class="col col-md-6">
+            <fieldset class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend"><label for="email" class="input-group-text">{{__("E-Mail Address")}}</label></div>
+                    <input type="email" name="email" id="email" placeholder="email@example.com" aria-placeholder="email@example.com">
+                </div>
+            </fieldset>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col col-md-6">
+            <fieldset class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend"><label for="password" class="input-group-text">Password</label></div>
+                    <input type="password" name="password" id="password" required aria-required="true">
+                </div>
+            </fieldset>
+        </div>
+        <div class="col col-md-6">
+            <fieldset class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend"><label for="passwordRepeat" class="input-group-text">{{__("Repeat password")}}</label></div>
+                    <input type="password" name="passwordRepeat" id="passwordRepeat">
+                </div>
+            </fieldset>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col col-md-6">
+            <fieldset class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend"><label for="birthDate" class="input-group-text">{{__("Birth date")}}</label></div>
+                    <input type="date" name="birthDate" id="birthDate" required aria-required="true" >
+                </div>
+            </fieldset>
+        </div>
+        <div class="col col-md-6">
+            <fieldset class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend"><label for="fiscalCode" class="input-group-text">{{__("Fiscal code")}}</label></div>
+                    <input type="text" name="fiscalCode" id="fiscalCode" placeholder="ABCXYZ12A34B123B" aria-placeholder="ABCXYZ12A34B123B">
+                </div>
+            </fieldset>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col col-md-6">
+            <fieldset class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend"><label for="phone" class="input-group-text">{{__("Phone number")}}</label></div>
+                    <input type="tel" name="phone" id="phone" required aria-required="true" placeholder="333 0123456789">
                 </div>
             </fieldset>
         </div>
@@ -30,4 +115,10 @@
         </div>
     </div>
 </form>
+<script>
+    $("#birthDate").datetimepicker({
+        format: 'yyyy-mm-dd',
+        autoclose: true
+    });
+</script>
 @endsection
