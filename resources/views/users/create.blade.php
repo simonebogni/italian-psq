871062d621bd8a1da48b09f26eae91780c996c28
Simone Bogni <simone.bogni@gmail.com>
2021-02-27 23:36:00 +0100

    Added errors to users.create view

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -7,13 +7,15 @@
     </div>
 </div>
 @if ($errors->any())
-{{dd($errors->all())}}
-    <div class="alert alert-danger">
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
         <ul>
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
         </ul>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 @endif
 <form action="{{route('users.store')}}" method="post">
