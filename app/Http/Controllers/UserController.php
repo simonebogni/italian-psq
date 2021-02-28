b9f42edc26c7ab997c1f45785747be911d81c3ad
Simone Bogni <simone.bogni@gmail.com>
2021-02-28 19:30:35 +0100

    Changed regex for password validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -74,7 +74,7 @@ class UserController extends Controller
             'role' => ['required'], 
             'name' => ['required', 'max:255'],
             'email' => ['required', 'email', 'unique:App\Models\User,email', 'max:255'],
-            'password' => ['required', 'regex:/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;,?*~$^+=<>]).{8,20}$/', 'confirmed'],
+            'password' => ['required', 'regex:/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–{}:;,?$+=<>]).{8,20}$/', 'confirmed'],
             'birthDate' => ['required', 'date'],
             'fiscalCode' => ['required', 'unique:App\Models\User,fiscal_code', 'regex:/^(?:[A-Z][AEIOU][AEIOUX]|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/i'],
             'phone' => ['required', 'numeric']
