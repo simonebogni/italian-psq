ffb0185f0c1d5e463638945415523acfcf27bcaf
Simone Bogni <simone.bogni@gmail.com>
2021-02-28 23:13:51 +0100

    Added bootstrap to email

--- resources/views/emails/users/create.blade.php
+++ resources/views/emails/users/create.blade.php
@@ -10,18 +10,17 @@
         <div class="col">
             <p>Benvenuto sul portale Italian PSQ!</p>
             <p>Il Suo account è stato creato con successo dal Suo pediatra o dell'amministratore di sistema.</p>
-            <p>Per accedere al sistema può cliccare sul bottone riportato qui di seguito.</p>
+            <p>Per accedere al sistema può cliccare sul link riportato qui di seguito:</p>
         </div>
     </div>
     <div class="row">
         <div class="col justify-content-center align-items-center">
-            <a role="button" class="btn btn-primary">Vai al sito</a>
+            <a href="http://italian-psq.herokuapp.com/">http://italian-psq.herokuapp.com/</a>
         </div>
     </div>
     <div class="row">
         <div class="col">
-            <p>In alternativa è possibile raggiungere il sito tramite il seguente link <a href="http://italian-psq.herokuapp.com/">http://italian-psq.herokuapp.com/</a></p>
-            <p>Di seguito riepiloghiamo i Suo dati e segnaliamo che la password temporanea assegnatale da chi ha creato l'account dovrà essere cambiata obbligatoriamente al primo accesso.</p>
+            <p>Di seguito riepiloghiamo i Suo dati e segnaliamo che la password temporanea assegnataLe da chi ha creato l'account dovrà essere cambiata obbligatoriamente al primo accesso.</p>
             <ul style="list-style: none;">
                 <li>Indirizzo email: {{$user->email}}</li>
                 <li>Password temporanea: {{$temporary_password}}</li>
